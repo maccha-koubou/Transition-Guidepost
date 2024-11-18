f086c12cba5d5d81548912f883a12ff9af0fd5c4
maccha_koubou <175473326+maccha-koubou@users.noreply.github.com>
2024-11-18 23:48:56 +0800

    [Track] Continue to work on the data type of Track function

--- app/src/main/java/com/maccha_koubou/transition_guidepost/model/RecordedData.kt
+++ app/src/main/java/com/maccha_koubou/transition_guidepost/model/RecordedData.kt
@@ -1,73 +1,78 @@
 package com.maccha_koubou.transition_guidepost.model
 
+import java.time.LocalTime
+
+// Class of a dosage data
+// unitWithoutInterval: The unit of each dose (NOT includes the interval)
+// interval: Days of interval
+class Dosage(dosage: Float, unitWithoutInterval: String, interval: Int)
+
+// Classes of single records
+open class Record()
+
+class CommonRecord(
+    val data: Float,
+    val time: LocalTime,
+): Record()
+
+class MedicationRecord(
+    val data: Float,
+    var dosage: Dosage,
+    val time: LocalTime,
+): Record()
+
+class CycleRecord(
+    val startTime: LocalTime,
+    val endTime: LocalTime?,
+): Record()
+
 open class RecordedData(
     open val name: String,
-    open var data: Float,
-    open val unit: Map<String, Float>, // Name of the unit & Ratio of all types to the first type
-    open val recommendationValue: Pair<Int, Int>, // Lower value & Higher value
-    open var isDataActivity: Boolean,
-    open var isAlarmActivity: Boolean,
-    open var alarmCycle: AlarmCycle,
-) {
+    open var isDataActive: Boolean,
+    open var alarmCycle: AlarmCycle?,
+){
 
 }
 
 data class TestData(
     override val name: String,
-    override var data: Float,
-    override val unit: Map<String, Float>,
-    override val recommendationValue: Pair<Int, Int>,
-    override var isDataActivity: Boolean,
-    override var isAlarmActivity: Boolean,
-    override var alarmCycle: AlarmCycle,
-): RecordedData(name, data, unit, recommendationValue, isDataActivity, isAlarmActivity, alarmCycle) {
+    val unit: Map<String, Float>, // Name of the unit & Ratio of all types to the first type
+    val recommendationValue: Pair<Float, Float>?, // Lower value & Higher value
+    override var isDataActive: Boolean,
+    override var alarmCycle: AlarmCycle?,
+): RecordedData(name, isDataActive, alarmCycle) {
 
 }
 
 data class MedicationData(
     override val name: String,
-    override var data: Float,
-    override val unit: Map<String, Float>,
-    override val recommendationValue: Pair<Int, Int>,
-    override var isDataActivity: Boolean,
-    override var isAlarmActivity: Boolean,
-    override var alarmCycle: AlarmCycle,
-): RecordedData(name, data, unit, recommendationValue, isDataActivity, isAlarmActivity, alarmCycle) {
-
+    val defaultDosage: Dosage,
+    override var isDataActive: Boolean,
+    override var alarmCycle: AlarmCycle?,
+): RecordedData(name, isDataActive, alarmCycle) {
+    var currentDosage = defaultDosage
 }
 
 data class TimePointData(
     override val name: String,
-    override var data: Float,
-    override val unit: Map<String, Float>,
-    override val recommendationValue: Pair<Int, Int>,
-    override var isDataActivity: Boolean,
-    override var isAlarmActivity: Boolean,
-    override var alarmCycle: AlarmCycle,
-): RecordedData(name, data, unit, recommendationValue, isDataActivity, isAlarmActivity, alarmCycle) {
+    override var isDataActive: Boolean,
+    override var alarmCycle: AlarmCycle?,
+): RecordedData(name, isDataActive, alarmCycle) {
 
 }
 
 data class BreastData(
     override val name: String,
-    override var data: Float,
-    override val unit: Map<String, Float>,
-    override val recommendationValue: Pair<Int, Int>,
-    override var isDataActivity: Boolean,
-    override var isAlarmActivity: Boolean,
-    override var alarmCycle: AlarmCycle,
-): RecordedData(name, data, unit, recommendationValue, isDataActivity, isAlarmActivity, alarmCycle) {
+    override var isDataActive: Boolean,
+    override var alarmCycle: AlarmCycle?,
+): RecordedData(name, isDataActive, alarmCycle) {
 
 }
 
 data class CycleData(
     override val name: String,
-    override var data: Float,
-    override val unit: Map<String, Float>,
-    override val recommendationValue: Pair<Int, Int>,
-    override var isDataActivity: Boolean,
-    override var isAlarmActivity: Boolean,
-    override var alarmCycle: AlarmCycle,
-): RecordedData(name, data, unit, recommendationValue, isDataActivity, isAlarmActivity, alarmCycle) {
+    override var isDataActive: Boolean,
+    override var alarmCycle: AlarmCycle?,
+): RecordedData(name, isDataActive, alarmCycle) {
 
 }
--- app/src/main/java/com/maccha_koubou/transition_guidepost/storage/RecordedData.kt
+++ app/src/main/java/com/maccha_koubou/transition_guidepost/storage/RecordedData.kt
@@ -1,2 +1,18 @@
 package com.maccha_koubou.transition_guidepost.storage
 
+import com.maccha_koubou.transition_guidepost.model.TestData
+
+//Default data of E2, T and PRL
+val e2Unit = mapOf("pmol/L" to 1f, "pg/mL" to 272.38f)
+val e2FeminizationRecommendation = Pair(367.1341f, 734.2683f)
+val e2MasculinizationRecommendation = Pair(36.71341f, 183.5670f)
+
+val tUnit = mapOf("nmol/L" to 1f, "ng/mL" to 0.28843f, "ng/dL" to 28.843f)
+val tFeminizationRecommendation = Pair(0.3467045f, 1.906875f)
+val tMasculinizationRecommendation = Pair(14f, 25.4f)
+
+val prlUnit = mapOf("mIU/L" to 1f, "ng/mL" to 0.04717f, "ng/dL" to 4.717f)
+val prlFeminizationRecommendation = Pair(101.55f, 493.96f)
+
+val e2Data = TestData("雌二醇", e2Unit, e2FeminizationRecommendation, true, null)
+val tData = TestData("睾酮", tUnit, tFeminizationRecommendation, true, null)
